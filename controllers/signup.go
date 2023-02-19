7849fb5eebb0d2be4c7501ac38c9f33cd05bf5e6
Luan Tran <tranminhluan1206@gmail.com>
2023-02-19 17:02:13 +0700

    feat: user now can create new account

--- controllers/signup.go
+++ controllers/signup.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"scylla/forms"
+
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -9,11 +12,30 @@ type SignupController struct {
 	beego.Controller
 }
 
-func (c *SignupController) Post() {
+func (c *SignupController) Signup() {
+	form := forms.RegistrationForm{}
+	flash := beego.NewFlash()
 
+	c.ParseForm(&form)
+
+	_, err := form.Save()
+	if err != nil {
+		flash.Error(err.Error())
+		flash.Store(&c.Controller)
+		c.Redirect("/signup", 302)
+		return
+	}
+
+	flash.Success("Wellcome to the town")
+	flash.Store(&c.Controller)
+	c.Redirect("/signup", 302)
 }
 
 func (c *SignupController) View() {
+	flash := beego.ReadFromRequest(&c.Controller)
+
+	fmt.Println(flash)
+
 	c.TplName = "signup.tpl"
 	c.Data["Title"] = "Create new account"
 }
---
+++ models/user.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+	"github.com/beego/beego/v2/client/orm"
+)
+
+type User struct {
+	Base
+
+	Email    string `orm:"unique"`
+	Password string
+}
+
+func init() {
+	orm.RegisterModel(new(User))
+}
+
+// AddUser insert a new User into database and returns
+// last inserted Id on success.
+func AddUser(m *User) (id int64, err error) {
+	o := orm.NewOrm()
+	id, err = o.Insert(m)
+	return
+}
+
+// GetUserById retrieves User by Id. Returns error if
+// Id doesn't exist
+func GetUserById(id int64) (v *User, err error) {
+	o := orm.NewOrm()
+	v = &User{
+		Base: Base{Id: id},
+	}
+	if err = o.QueryTable(new(User)).Filter("Id", id).RelatedSel().One(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+func (u *User) Existed() bool {
+	if user, _ := GetUserByEmail(u.Email); user == nil {
+		return false
+	}
+
+	return true
+}
+
+func GetUserByEmail(email string) (v *User, err error) {
+	o := orm.NewOrm()
+	var user User
+
+	if err = o.QueryTable("user").Filter("email", email).One(&user); err == nil {
+		return &user, nil
+	}
+
+	return nil, err
+}
