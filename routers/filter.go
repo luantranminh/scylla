885bb44455b60f95a953e21e0ccdc53619ad4aae
Luan Tran <tranminhluan1206@gmail.com>
2023-02-21 22:00:20 +0700

    fix: use namespace to more clear on routes

--- routers/filter.go
+++ routers/filter.go
@@ -1,23 +1,12 @@
 package routers
 
 import (
-	"strings"
-
-	beego "github.com/beego/beego/v2/server/web"
 	"github.com/beego/beego/v2/server/web/context"
 )
 
-func init() {
-	var FilterUser = func(ctx *context.Context) {
-		if strings.HasPrefix(ctx.Input.URL(), "/login") {
-			return
-		}
-
-		_, ok := ctx.Input.Session("UserID").(int)
-		if !ok {
-			ctx.Redirect(302, "/login")
-		}
+func Auth(ctx *context.Context) {
+	userID := ctx.Input.Session("UserID")
+	if userID == nil {
+		ctx.Redirect(302, "/login")
 	}
-
-	beego.InsertFilter("/home", beego.BeforeRouter, FilterUser)
 }
